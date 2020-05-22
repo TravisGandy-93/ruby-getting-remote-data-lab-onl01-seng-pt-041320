b6f54c6eca5b9460578289c2bc5fb42e0d298f20
Travis Gandy <travis_gandy@yahoo.com>
2020-05-22 23:37:41 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,12 +4,13 @@ require 'open-uri'
 require 'json'
 
 class GetRequester
-  
+  attr_accessor :url_string
   def initialize(url_string)
+      @url_string = url_string
   end 
   
   def get_response_body
-    uri = URI.parse()
+    uri = URI.parse(@url_string)
     response = Net::HTTP.get_response(uri)
   	response.body
   end 
